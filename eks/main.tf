fb74cdfa9f5fceb039c2f29ce6cdc5cc770ce6cf
tlandaeta-exos <tomas.landaeta@exosfinantial.com>
2022-10-17 10:42:32 -0300

    update base

--- eks/main.tf
+++ eks/main.tf
@@ -10,33 +10,90 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  profile = "default"
 }
 
-module "eks_blueprints" {
+data "aws_eks_cluster" "cluster" {
+  name = "infra-tools"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "infra-tools"
+}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+module "infra_tools" {
   source          = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.12.0"
   cluster_version = "1.23"
-  cluster_name    = "eks-tests-1"
+  cluster_name    = "infra-tools"
   vpc_id          = "vpc-095099ed4074eb82e"
   private_subnet_ids = ["subnet-04ab150e2266c19aa",
-    "subnet-014dfafdd6dfbd14f",
-    "subnet-006ebe6ba3b8643c7",
-  "subnet-0afec5da6acc6047a"]
+    "subnet-006ebe6ba3b8643c7"]
   managed_node_groups = {
     mg_t3 = {
       node_group_name = "managed-ondemand"
       instance_types  = ["t3.large"]
+      desired_size    = 1
+      max_size        = 1
+      min_size        = 1
       subnet_ids = ["subnet-04ab150e2266c19aa",
-        "subnet-014dfafdd6dfbd14f",
-        "subnet-006ebe6ba3b8643c7",
-      "subnet-0afec5da6acc6047a"]
+        "subnet-006ebe6ba3b8643c7"]
     }
   }
 }
 
-module "eks_blueprints_kubernetes_addons" {
+module "infra_tools_addons" {
   source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.12.0"
 
   eks_cluster_id = module.eks_blueprints.eks_cluster_id
+
+  # EKS Addons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+
+  #K8s Add-ons
+  enable_aws_load_balancer_controller = true
+  enable_cluster_autoscaler           = true
+  enable_metrics_server               = true
+  enable_prometheus                   = true
+
+  prometheus_helm_config = {
+    chart = "kube-prometheus-stack"
+    values = [templatefile("./k8s_values/shared.yaml", {}), 
+              templatefile("./k8s_values/observer/observer.yaml", {})]
+  }
+}
+
+module "observee" {
+  source          = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.12.0"
+  cluster_version = "1.23"
+  cluster_name    = "observee"
+  vpc_id          = "vpc-095099ed4074eb82e"
+  private_subnet_ids = ["subnet-04ab150e2266c19aa",
+    "subnet-006ebe6ba3b8643c7"]
+  managed_node_groups = {
+    mg_t3 = {
+      node_group_name = "managed-ondemand"
+      instance_types  = ["t3.large"]
+      desired_size    = 1
+      max_size        = 1
+      min_size        = 1
+      subnet_ids = ["subnet-04ab150e2266c19aa",
+        "subnet-006ebe6ba3b8643c7"]
+    }
+  }
+}
+
+module "observee_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.12.0"
+
+  eks_cluster_id = module.observee.eks_cluster_id
 
   # EKS Addons
   enable_amazon_eks_vpc_cni            = true
